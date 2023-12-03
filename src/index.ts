754d4adcdb3d4b5c1584cfce15ec11b9efc3a155
haha58 <11461089222@qq.com>
2023-12-03 14:27:04 +0800

    add:husky

--- src/index.ts
+++ src/index.ts
@@ -1,18 +1,17 @@
 import { App } from 'vue'
 import LText from './components/l-text/index.vue'
 
-const components=[LText]
+const components = [LText]
 
-//遍历components，并注册全局组件
-const install =(app:App)=>{
-
-  components.forEach(component=>{
+// 遍历components，并注册全局组件
+const install = (app: App) => {
+  components.forEach(component => {
     console.log(component)
-    app.component(component.name,component)
+    app.component(component.name, component)
   })
 }
-  //按需导出
+// 按需导出
 export { LText }
 
-//整体导出
-export default {install}
+// 整体导出
+export default { install }
