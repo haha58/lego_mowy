3f33269ead0af3100f2a769b45ac4fb81d9abd8c
haha58 <11461089222@qq.com>
2023-12-03 15:58:21 +0800

    fix:test new husky

--- scripts/verify-commit.js
+++ scripts/verify-commit.js
@@ -1,16 +1,16 @@
-const chalk = require('chalk')
-const path = require('path')
-const msg = require('fs').readFileSync(path.resolve('.git/COMMIT_EDITMSG'), 'utf-8').trim()
+import chalk from 'chalk'
+import path from 'path'
+import fs from 'fs'
+const msg = fs.readFileSync(path.resolve('.git/COMMIT_EDITMSG'), 'utf-8').trim()
 
-const commitRE = /^(\d+\r?\n(?:M|m)essage: .{1,50})/
+const commitRE = /^(fix|update):.{1,50}/
 
 if (!commitRE.test(msg)) {
   console.log()
   console.error(
     `    ${chalk.bgRed.white(' ERROR ')} ${chalk.red(`Invalid commit message format.`)}\n\n` +
       chalk.red(`    Proper commit message format as below. Example:\n\n`) +
-      `    ${chalk.green(`S2222`)}\n` +
-      `    ${chalk.green(`Message: feat(router): 修改文案字体大小`)}\n\n`
+      `    ${chalk.green(`fix:  修改文案字体大小`)}\n\n`
   )
   process.exit(1)
 }
