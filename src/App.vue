7d48820617eb21fcfc2fcc6e8ab8687f04fbe79f
haha58 <1461089222@qq.com>
2024-03-13 22:37:18 +0800

    update:version

--- src/App.vue
+++ src/App.vue
@@ -1,10 +1,14 @@
-<template><div>a</div></template>
+<template>
+  <l-image src="http://static.imooc-lego.com/upload-files/logo-white-735536.png" />
+</template>
 
 <script lang="ts">
 import { defineComponent } from 'vue'
+import { LImage } from './components'
 
 export default defineComponent({
   name: 'App',
+  components: { LImage },
   setup() {
     return {}
   }
